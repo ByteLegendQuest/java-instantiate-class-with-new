1496926977425ba39a1a55cd6ea9b3bf5e4a9f8c
Opalcalypse <v.a.red@outlook.com>
2022-07-19 07:34:15 +0000

    Update at 2022-07-19T07:34:15.582062Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -12,5 +12,7 @@ public class Home {
     }
 
     // Create a new Cat instance and return it
-    public static Cat createNewCat() {}
+    public static Cat createNewCat() {
+        Cat catObject = new Cat;
+    }
 }
