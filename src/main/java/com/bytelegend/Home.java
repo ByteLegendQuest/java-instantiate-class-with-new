1e8a227b19f15586124825ec43521f628f0f214f
darqlt <stungys@gmail.com>
2023-02-20 22:13:02 +0000

    Update at 2023-02-20T22:13:02.424527Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -12,5 +12,11 @@ public class Home {
     }
 
     // Create a new Cat instance and return it
-    public static Cat createNewCat() {}
+    public static Cat createNewCat() {
+        if (cat != null){
+            return cat;
+        }
+        cat = new Cat();
+        return cat;
+    }
 }
